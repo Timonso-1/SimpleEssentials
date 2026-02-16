da2c30f06aecd55ebe107c124d0d7210ca35f8f6
Timonso <management@timonso.de>
2026-02-16 19:17:03 +0100

    Add fly command with permission handling and messaging

---
+++ src/main/kotlin/de/timonso/simpleCore/command/FlyCommand.kt
@@ -0,0 +1,68 @@
+package de.timonso.simpleCore.command
+
+import de.timonso.simpleCore.util.prefix.PrefixUtil
+import de.timonso.simpleCore.util.permission.SimpleCorePermissionRegistry
+import dev.jorel.commandapi.kotlindsl.*
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
+
+fun flyCommand() = commandTree("fly") {
+    withPermission(SimpleCorePermissionRegistry.FLY_COMMAND)
+    playerExecutor { player, _ ->
+
+        if (player.allowFlight) {
+            player.allowFlight = false
+            player.isFlying = false
+
+            player.sendText {
+                append(PrefixUtil.PREFIX)
+                success("Du kannst nun nicht mehr fliegen.")
+            }
+        } else {
+            player.allowFlight = true
+            player.isFlying = true
+
+            player.sendText {
+                append(PrefixUtil.PREFIX)
+                success("Du kannst nun fliegen.")
+            }
+        }
+    }
+
+    entitySelectorArgumentOnePlayer("player") {
+        withPermission(SimpleCorePermissionRegistry.FLY_COMMAND_OTHERS)
+        anyExecutor { executor, args ->
+            val target = args[0] as Player
+
+            if (target.allowFlight) {
+                target.allowFlight = false
+                target.isFlying = false
+
+                executor.sendText {
+                    append(PrefixUtil.PREFIX)
+                    variableValue(target.name)
+                    appendSpace()
+                    success("kann nun nicht mehr fliegen.")
+                }
+                target.sendText {
+                    append(PrefixUtil.PREFIX)
+                    info("Du kannst nun nicht mehr fliegen.")
+                }
+            } else {
+                target.allowFlight = true
+                target.isFlying = true
+
+                executor.sendText {
+                    append(PrefixUtil.PREFIX)
+                    variableValue(target.name)
+                    appendSpace()
+                    success("kann nun fliegen.")
+                }
+                target.sendText {
+                    append(PrefixUtil.PREFIX)
+                    info("Du kannst nun fliegen.")
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/de/timonso/simpleCore/util/permission/SimpleCorePermissionRegistry.kt
@@ -0,0 +1,10 @@
+package de.timonso.simpleCore.util.permission
+
+import dev.slne.surf.surfapi.bukkit.api.permission.PermissionRegistry
+
+object SimpleCorePermissionRegistry : PermissionRegistry() {
+    private const val PREFIX = "simplecore"
+
+    val FLY_COMMAND = create("$PREFIX.fly.command")
+    val FLY_COMMAND_OTHERS = create("$PREFIX.fly.others.command")
+}
